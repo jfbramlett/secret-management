676877fd0cfb7fc069cbe0c9b5595d8824202854
Bramlett, John <john.bramlett@hbo.com>
2017-11-21 16:02:21 -0500

    Fixing path

--- docker/spring-config/start_spring-config.sh
+++ docker/spring-config/start_spring-config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it -p 8888:8888 -v /Users/jbramlet/git/secret-management/docker/spring-config/config:/config hyness/spring-cloud-config-server
+docker run -it -p 8888:8888 -v ./config:/config hyness/spring-cloud-config-server
