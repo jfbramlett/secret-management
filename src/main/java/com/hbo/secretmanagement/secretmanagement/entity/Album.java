e1c2ad1bf0452edd0d00e9a819d309a57d96fa31
Bramlett, John <john.bramlett@hbo.com>
2018-01-05 12:38:28 -0500

    Updating after successful usage

--- src/main/java/com/hbo/secretmanagement/secretmanagement/entity/Album.java
+++ src/main/java/com/hbo/secretmanagement/secretmanagement/entity/Album.java
@@ -1,15 +1,26 @@
 package com.hbo.secretmanagement.secretmanagement.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "albums")
 public class Album {
     @Id
+    private String id;
     private String title;
     private String artist;
     private int year;
 
     public Album() {
 
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
